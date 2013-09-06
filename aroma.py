74a373d4ad58613e34029fd6372e48b2c984f0a9
JD Isaacks <jd@jisaacks.com>
2013-09-05 21:47:23 -0400

    Allow User to specify compile to extension in settings

--- aroma.py
+++ aroma.py
@@ -1,11 +1,33 @@
 import sublime, sublime_plugin, subprocess, os, signal
 
+class AromaHelper:
+  
+  @staticmethod
+  def settings(key):
+    if not hasattr(AromaHelper, '_settings'):
+      AromaHelper._settings = sublime.load_settings('Aroma.sublime-settings')
+
+    return AromaHelper._settings.get(key)
+
+#------
+
 class AromaCompiler:
   _watching = {}
+  @staticmethod
+  def run(*opts):
+    args = ["aroma"]
+    for opt in opts:
+      args.append(opt)
+    ext = AromaHelper.settings('use_extension')
+    if ext:
+      args.append("-e")
+      args.append(ext)
+    print ("Aroma Process Args",args)
+    return subprocess.Popen(args, stdout=subprocess.PIPE)
 
   @staticmethod
   def compile(source):
-    pro = subprocess.Popen(["aroma", "-c", source], stdout=subprocess.PIPE)
+    pro = AromaCompiler.run("-c", source)
     (out, err) = pro.communicate()
     if out:
       print ("program output:", out)
@@ -14,7 +36,7 @@ class AromaCompiler:
 
   @staticmethod
   def watch(source):
-    pro = subprocess.Popen(["aroma", "-wc", source], stdout=subprocess.PIPE)
+    pro = AromaCompiler.run("-wc", source)
     print ("Aroma Watching:",source)
     AromaCompiler._watching[source] = pro.pid
 
